337e43d9683976b94ee89ec6247be7f9b78018ff
Death960 <moonshine7960@gmail.com.com>
2023-12-07 17:17:11 +0300

    Db

---
+++ db/models/question.js
@@ -0,0 +1,46 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Question extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Category }) {
+      this.belongsTo(Category, { foreignKey: 'categoryId' });
+    }
+  }
+  Question.init(
+    {
+      quest: {
+        allowNull: false,
+
+        type: DataTypes.TEXT,
+      },
+      answer: {
+        allowNull: false,
+
+        type: DataTypes.TEXT,
+      },
+      img: {
+        type: DataTypes.TEXT,
+      },
+      categoryId: {
+        allowNull: false,
+
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        onDelete: 'cascade',
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Question',
+    }
+  );
+  return Question;
+};
---
+++ db/models/user.js
@@ -0,0 +1,41 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      name: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      score: {
+        allowNull: false,
+
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+  return User;
+};
