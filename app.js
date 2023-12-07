d923e51db2152273f919bd29ba46a8946eab986b
meechisdeadd <ifuckyourgrandma@mail.ru>
2023-12-07 14:43:45 +0300

    StartProject

---
+++ app.js
@@ -0,0 +1,14 @@
+require('@babel/register');
+const express = require('express');
+const app = express();
+const path = require('path');
+const ssr = require('./middlewares/ssr');
+
+app.use(ssr);
+app.use(express.static(path.join(__dirname, './public')));
+app.use(express.urlencoded({ extended: true })); //даем разрешение на чтение данных из body из формы
+app.use(express.json());
+
+// app.use('/', indexRouter);
+
+app.listen(3000, () => console.log('Все работает'));
